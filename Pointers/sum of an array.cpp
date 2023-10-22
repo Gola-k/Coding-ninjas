c6a3995004f90e5ab1b670348a2f26ce06ba7852
Gola-k <kunalgola426@gmail.com>
2023-10-22 10:29:31 +0530

    2nd commit

--- Pointers/sum of an array.cpp
+++ Pointers/sum of an array.cpp
@@ -22,3 +22,26 @@
 
 // Code:
 
+#include <iostream>
+using namespace std;
+
+int sum(int input[], int n) {
+    if(n==0) return 0;
+    return input[0] + sum(input+1, n-1);
+}
+
+int main() {
+    int n;
+    cin >> n;
+    int *input = new int[n];
+    for(int i=0; i<n; i++) {
+        cin >> input[i];
+    }
+    cout << sum(input, n) << endl;
+    delete [] input;
+}
+
+// Output:
+// 3
+// 9 8 9
+// 26
